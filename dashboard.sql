5cbdce993be85993b9afbc7c2f08e698b8ef78a1
Matvey Zhukov <matvey532@yandex.ru>
2024-08-16 15:43:25 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -68,7 +68,7 @@ with weekly_visits as (
         extract(dow from s.visit_date) as day_of_week,
         count(distinct s.visitor_id) as visitor_count
     from sessions as s
-    group by extract(dow from s.visit_date)
+    group by day_of_week
 )
 
 select
@@ -114,10 +114,9 @@ with sales as (
             order by s.visit_date::date desc
         ) as sale_count
     from sessions as s
-    left join
-        leads as l on
+    left join leads as l on
         s.visitor_id = l.visitor_id and
-		s.visit_date::date <= l.created_at::date
+        s.visit_date::date <= l.created_at::date
     where s.medium != 'organic'
 ),
 
@@ -163,8 +162,7 @@ tab as (
         ) as purchases_count,
         sum(s.amount) as revenue
     from sales as s
-    left join 
-        costs as c on
+    left join costs as c on
         s.source = c.utm_source and
         s.medium = c.utm_medium and
         s.campaign = c.utm_campaign and
@@ -207,10 +205,9 @@ select
     coalesce(
         case
             when sum(tab.total_cost) = 0 then 0
-            else 
-                round(
-                	(sum(tab.revenue) - sum(tab.total_cost)) / 
-                	sum(tab.total_cost) * 100, 
+            else round(
+                (sum(tab.revenue) - sum(tab.total_cost)) / 
+                sum(tab.total_cost) * 100, 
                 2
             )
         end,
@@ -219,6 +216,7 @@ select
 from tab
 where tab.utm_source in ('vk', 'yandex')
 group by tab.utm_source;
+
 
 -- Расчет метрик (cpu, cpl, cppu, roi) для utm_source, utm_medium и utm_campaign
 with sales as (
@@ -238,8 +236,7 @@ with sales as (
             order by s.visit_date::date desc
         ) as sale_count
     from sessions as s
-    left join
-    	leads as l on
+    left join leads as l on
         s.visitor_id = l.visitor_id and
         s.visit_date::date <= l.created_at::date
     where s.medium != 'organic'
@@ -257,7 +254,7 @@ costs as (
         vk.campaign_date::date,
         vk.utm_source,
         vk.utm_medium,
-        vk.utm_campaig
+        vk.utm_campaign
     union all
     select
         ya.campaign_date::date as campaign_date,
@@ -287,8 +284,7 @@ tab as (
         ) as purchases_count,
         sum(s.amount) as revenue
     from sales as s
-    left join
-        costs as c on
+    left join costs as c on
         s.source = c.utm_source and
         s.medium = c.utm_medium and
         s.campaign = c.utm_campaign and
@@ -333,10 +329,9 @@ select
     coalesce(
         case
             when sum(tab.total_cost) = 0 then 0
-            else
-				round(
-					(sum(tab.revenue) - sum(tab.total_cost)) / 
-					sum(tab.total_cost) * 100, 
+            else round(
+                (sum(tab.revenue) - sum(tab.total_cost)) / 
+                sum(tab.total_cost) * 100, 
                 2
             )
         end,
@@ -345,6 +340,7 @@ select
 from tab
 where tab.utm_source in ('vk', 'yandex')
 group by tab.utm_source, tab.utm_medium, tab.utm_campaign;
+
 
 -- Расчет конверсий
 with sales as (
