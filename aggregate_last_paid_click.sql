3bc0c7f7ada733e9336814d28682cb31a399191a
Matvey Zhukov <matvey532@yandex.ru>
2024-08-16 12:24:20 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -10,13 +10,10 @@ with sales as (
         l.created_at,
         l.closing_reason,
         l.status_id,
-        row_number()
-        over (partition by s.visitor_id order by s.visit_date desc)
-        as sale_count
+        row_number() over (partition by s.visitor_id order by s.visit_date desc) as sale_count
     from sessions as s
-    left join
-        leads as l
-        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    left join leads as l
+    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
     where s.medium != 'organic'
 ),
 
@@ -28,8 +25,7 @@ costs as (
         vk.utm_campaign,
         sum(vk.daily_spent) as daily_spent
     from vk_ads as vk
-    group by
-        vk.campaign_date::date, vk.utm_source, vk.utm_medium, vk.utm_campaign
+    group by vk.campaign_date::date, vk.utm_source, vk.utm_medium, vk.utm_campaign
     union all
     select
         ya.campaign_date::date,
@@ -38,8 +34,7 @@ costs as (
         ya.utm_campaign,
         sum(ya.daily_spent) as daily_spent
     from ya_ads as ya
-    group by
-        ya.campaign_date::date, ya.utm_source, ya.utm_medium, ya.utm_campaign
+    group by ya.campaign_date::date, ya.utm_source, ya.utm_medium, ya.utm_campaign
 )
 
 select
@@ -55,15 +50,18 @@ select
     ) as purchases_count,
     sum(s.amount) as revenue
 from sales as s
-left join
-    costs as c
-    on
-        s.source = c.utm_source
-        and s.medium = c.utm_medium
-        and s.campaign = c.utm_campaign
-        and s.visit_date::date = c.campaign_date
+left join costs as c
+on
+    s.source = c.utm_source
+    and s.medium = c.utm_medium
+    and s.campaign = c.utm_campaign
+    and s.visit_date::date = c.campaign_date
 where s.sale_count = 1
 group by s.visit_date::date, s.source, s.medium, s.campaign, c.daily_spent
 order by
-    revenue desc nulls last, s.visit_date::date asc, visitors_count desc, 
-   	utm_source asc, utm_medium asc, utm_campaign asc;
+    revenue desc nulls last,
+    s.visit_date::date asc,
+    visitors_count desc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc;
