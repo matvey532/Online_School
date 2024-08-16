ad7187cd672b30145004708e062eaff2442ae6f4
Matvey Zhukov <matvey532@yandex.ru>
2024-08-16 12:27:32 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -10,10 +10,14 @@ with sales as (
         l.created_at,
         l.closing_reason,
         l.status_id,
-        row_number() over (partition by s.visitor_id order by s.visit_date desc) as sale_count
+        row_number() over (
+            partition by s.visitor_id 
+            order by s.visit_date desc
+        ) as sale_count
     from sessions as s
     left join leads as l
-    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+        on s.visitor_id = l.visitor_id 
+        and s.visit_date <= l.created_at
     where s.medium != 'organic'
 ),
 
@@ -25,7 +29,11 @@ costs as (
         vk.utm_campaign,
         sum(vk.daily_spent) as daily_spent
     from vk_ads as vk
-    group by vk.campaign_date::date, vk.utm_source, vk.utm_medium, vk.utm_campaign
+    group by 
+        vk.campaign_date::date, 
+        vk.utm_source, 
+        vk.utm_medium, 
+        vk.utm_campaign
     union all
     select
         ya.campaign_date::date,
@@ -34,7 +42,11 @@ costs as (
         ya.utm_campaign,
         sum(ya.daily_spent) as daily_spent
     from ya_ads as ya
-    group by ya.campaign_date::date, ya.utm_source, ya.utm_medium, ya.utm_campaign
+    group by 
+        ya.campaign_date::date, 
+        ya.utm_source, 
+        ya.utm_medium, 
+        ya.utm_campaign
 )
 
 select
@@ -46,18 +58,23 @@ select
     count(s.visitor_id) as visitors_count,
     count(s.lead_id) as leads_count,
     count(s.lead_id) filter (
-        where s.closing_reason = 'Успешно реализовано' or s.status_id = 142
+        where s.closing_reason = 'Успешно реализовано' 
+        or s.status_id = 142
     ) as purchases_count,
     sum(s.amount) as revenue
 from sales as s
 left join costs as c
-on
-    s.source = c.utm_source
+    on s.source = c.utm_source
     and s.medium = c.utm_medium
     and s.campaign = c.utm_campaign
     and s.visit_date::date = c.campaign_date
 where s.sale_count = 1
-group by s.visit_date::date, s.source, s.medium, s.campaign, c.daily_spent
+group by 
+    s.visit_date::date, 
+    s.source, 
+    s.medium, 
+    s.campaign, 
+    c.daily_spent
 order by
     revenue desc nulls last,
     s.visit_date::date asc,
